d1394feb9b014fafb3e8aebb8b82f232fc358fc7
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-05-17 23:36:37 +0300

    Delete src/main/java/ua/en/kosse/oksana/hillel/hw7 directory

--- src/main/java/ua/en/kosse/oksana/hillel/hw7/exception/ArrayDataException.java
+++
@@ -1,8 +0,0 @@
-package ua.en.kosse.oksana.hillel.hw7.exception;
-
-public class ArrayDataException extends Exception{
-    public ArrayDataException(String message){
-        super(message);
-
-    }
-}
