96a3130888a8fde24bc34ded16eddab92b93553c
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-06-25 20:17:09 +0300

    Delete src/main/java/ua/en/kosse/oksana/hillel/hw5 directory

--- src/main/java/ua/en/kosse/oksana/hillel/hw5/number/Numbers.java
+++
@@ -1,8 +0,0 @@
-package ua.en.kosse.oksana.hillel.hw5.number;
-
-public class Numbers {
-
-    public String compareNumbers(int valA, int valB) {
-        return valA >= valB ?  "a >= b" : "a < b";
-    }
-}
