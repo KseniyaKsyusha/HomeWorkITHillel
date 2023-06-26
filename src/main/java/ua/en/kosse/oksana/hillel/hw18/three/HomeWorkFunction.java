b2a279b896b26a289db6850c57a3e96568eab21e
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-06-26 20:08:27 +0300

    Add files via upload

--- src/main/java/ua/en/kosse/oksana/hillel/hw18/three/HomeWorkFunction.java
+++ src/main/java/ua/en/kosse/oksana/hillel/hw18/three/HomeWorkFunction.java
@@ -3,6 +3,7 @@ package ua.en.kosse.oksana.hillel.hw18.three;
 import ua.en.kosse.oksana.hillel.hw18.one.Product;
 import ua.en.kosse.oksana.hillel.hw18.one.TypeProduct;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -21,17 +22,26 @@ public class HomeWorkFunction {
         printBook(productList);
         System.out.println("----------------------------");
         takeProduct(productList, BOOK);
+        System.out.println("-----------------------------");
+        System.out.println(takeProductFun(productList, BOOK));
     }
 
     public static void takeProduct(List<Product> products, TypeProduct type) {
+
         try {
             Product min = products.stream().filter(booking -> booking.getType().equals(type)).
-                                    min(Product::compareTo).get();
+                    min(Product::compareTo).orElseThrow(Exception::new);
             System.out.println(min.getType() + ": min price => " + min.getPrices());
-        } catch (NoSuchElementException e) {
+        } catch (Exception e) {
             System.out.println("Product [Category" + type + "] not found]");
         }
+
     }
 
+    public static Product takeProductFun(List<Product> products, TypeProduct type) {
+        return products.stream().filter(booking -> booking.getType().equals(type)).
+                min(Comparator.comparing(Product::getPrices)).
+                orElseThrow();
+    }
 
 }
