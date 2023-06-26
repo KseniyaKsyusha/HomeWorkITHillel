4793b2558a62d19a8aeca004a8d7a127ee51dc07
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-06-26 20:06:16 +0300

    Add files via upload

--- src/main/java/ua/en/kosse/oksana/hillel/hw18/four/HomeWorkFunction.java
+++ src/main/java/ua/en/kosse/oksana/hillel/hw18/four/HomeWorkFunction.java
@@ -1,14 +1,12 @@
 package ua.en.kosse.oksana.hillel.hw18.four;
 
 import ua.en.kosse.oksana.hillel.hw18.one.Product;
-import ua.en.kosse.oksana.hillel.hw18.one.TypeProduct;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static ua.en.kosse.oksana.hillel.hw18.four.ProductDate.productDate;
-import static ua.en.kosse.oksana.hillel.hw18.one.HomeWorkFunction.printBook;
-import static ua.en.kosse.oksana.hillel.hw18.one.TypeProduct.BOOK;
 
 
 public class HomeWorkFunction {
@@ -19,17 +17,27 @@ public class HomeWorkFunction {
         //4.2 Реалізувати метод отримання трьох останніх доданих продуктів
         printProduct(productList);
         System.out.println("----------------------------");
-        takeProduct(productList);
+        takeProduct(productList).forEach(System.out::println);
+        System.out.println("----------------------------");
+        takeProductLimit(productList).forEach(System.out::println);
+
     }
 
-    public static void takeProduct(List<Product> products) {
-        products.stream().sorted(Comparator.comparing(Product::getDateShop)).
-                          skip(products.size() - 3).
-                          forEach(System.out::println);
+    public static List<Product> takeProduct(List<Product> products) {
+        return products.stream().sorted(Comparator.comparing(Product::getDateShop)).
+                skip(products.size() - 3).
+                collect(Collectors.toList());
     }
+
+    public static List<Product> takeProductLimit(List<Product> products) {
+        return products.stream().sorted(Comparator.comparing(Product::getDateShop).reversed())
+                .limit(3)
+                .collect(Collectors.toList());
+    }
+
     public static void printProduct(List<Product> products) {
         products.stream().sorted(Comparator.comparing(Product::getDateShop)).
-                          forEach(System.out::println);
+                forEach(System.out::println);
     }
 }
 
