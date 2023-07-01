9d130066e8c8990927e898eaae0bffdf69773406
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-07-01 23:08:31 +0300

    Add files via upload

---
+++ src/main/java/ua/en/kosse/oksana/hillel/hw19/exception/NumberOfAnnotationException.java
@@ -0,0 +1,7 @@
+package ua.en.kosse.oksana.hillel.hw19.exception;
+
+public class NumberOfAnnotationException extends Exception {
+    public NumberOfAnnotationException(){
+        System.out.println("Wrong number of annotations.");
+    }
+}
