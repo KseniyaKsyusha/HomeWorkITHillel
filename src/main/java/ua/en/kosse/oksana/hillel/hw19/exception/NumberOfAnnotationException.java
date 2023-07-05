02d81642bacdf44be36089d75258318fd664dfe6
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-07-05 10:16:59 +0300

    Delete src/main/java/ua/en/kosse/oksana/hillel/hw19 directory

--- src/main/java/ua/en/kosse/oksana/hillel/hw19/exception/NumberOfAnnotationException.java
+++
@@ -1,7 +0,0 @@
-package ua.en.kosse.oksana.hillel.hw19.exception;
-
-public class NumberOfAnnotationException extends Exception {
-    public NumberOfAnnotationException(){
-        System.out.println("Wrong number of annotations.");
-    }
-}
