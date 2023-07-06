6f43e1e2b3bcb53d8201996e52844cafd012c457
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-07-07 01:00:31 +0300

    Add files via upload

--- src/main/java/ua/en/kosse/oksana/hillel/hw21/Program.java
+++ src/main/java/ua/en/kosse/oksana/hillel/hw21/Program.java
@@ -1,9 +1,9 @@
-package ua.en.kosse.oksana.hillel.hw21;
+package ua.en.kosse.oksana.hillel.hw20;
 
 public class Program{
 
     public static void main(String[] args) {
-        //java -classpath c:\Java\mysql-connector-java-8.0.11.jar;c:\Java Program
+
         try{
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
             System.out.println("Connection succesfull!");
