5654ac2d2d05941a616e820ffe9be00d7638b0f8
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-07-24 13:59:45 +0300

    Add files via upload

--- src/main/java/ua/en/kosse/oksana/hillel/hw23/strategy/figure/SquareTriangle.java
+++ src/main/java/ua/en/kosse/oksana/hillel/hw23/strategy/figure/SquareTriangle.java
@@ -2,9 +2,9 @@ package ua.en.kosse.oksana.hillel.hw23.strategy.figure;
 
 import ua.en.kosse.oksana.hillel.hw23.strategy.SquareStrategy;
 
-public class SquareRectangle implements SquareStrategy {
+public class SquareTriangle implements SquareStrategy {
     @Override
-    public double square(double sideA, double sideB) {
-        return sideA*sideB;
+    public double square(double height, double side) {
+        return height*side*1/2;
     }
 }
