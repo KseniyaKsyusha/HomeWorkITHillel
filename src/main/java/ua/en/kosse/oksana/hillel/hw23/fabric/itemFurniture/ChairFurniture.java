8b1baa8bb54ff0cc6edfa8018fb0566aaa46c06a
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-07-24 13:54:26 +0300

    Add files via upload

---
+++ src/main/java/ua/en/kosse/oksana/hillel/hw23/fabric/itemFurniture/ChairFurniture.java
@@ -0,0 +1,8 @@
+package ua.en.kosse.oksana.hillel.hw23.fabric.itemFurniture;
+
+public class ChairFurniture implements Furniture {
+    @Override
+    public void doFurniture() {
+        System.out.println("You ordered a chair!");
+    }
+}
--- src/main/java/ua/en/kosse/oksana/hillel/hw23/fabric/itemFurniture/Furniture.java
+++ src/main/java/ua/en/kosse/oksana/hillel/hw23/fabric/itemFurniture/Furniture.java
@@ -1,1 +1,5 @@
+package ua.en.kosse.oksana.hillel.hw23.fabric.itemFurniture;
 
+public interface Furniture {
+    void doFurniture();
+}
