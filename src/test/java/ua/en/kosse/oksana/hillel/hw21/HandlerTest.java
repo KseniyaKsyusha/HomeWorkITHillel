65c5faf070bfc6078c0e6167660e6e7ea5e90708
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-12-04 22:10:11 +0200

    Update HandlerTest.java

--- src/test/java/ua/en/kosse/oksana/hillel/hw21/HandlerTest.java
+++ src/test/java/ua/en/kosse/oksana/hillel/hw21/HandlerTest.java
@@ -1,1 +1,36 @@
+package ua.en.kosse.oksana.hillel.hw21;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ua.en.kosse.oksana.hillel.hw21.server.Handler;
+
+public class HandlerTest {
+    @Test
+    public void Handler_broadCastMessage_zero_messages() {
+        Handler handler = new Handler(null, null);
+        Assertions.assertEquals(0, handler.broadCastMessage(null));
+    }
+
+    @Test
+    public void Handler_run_throw_exeption() {
+        Handler handler = new Handler(null, null);
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            handler.run();
+        });
+    }
+
+    @Test
+    public void Handler_fileCommandProcessor_incorrect_metadata() {
+        Handler handler = new Handler(null, null);
+        Assertions.assertEquals(
+                2,
+                handler.fileCommandProcessor("a:b"));
+        Assertions.assertEquals(
+                2,
+                handler.fileCommandProcessor("a:b:0"));
+        Assertions.assertEquals(
+                2,
+                handler.fileCommandProcessor("a::100"));
+    }
+}
 
