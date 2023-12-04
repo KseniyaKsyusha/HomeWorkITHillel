d2bb90e8d3310e7b3fd03ab0ecf9b8713d10c3de
KseniyaKsyusha <109752337+KseniyaKsyusha@users.noreply.github.com>
2023-12-04 22:06:09 +0200

    Update HomeWorkClientTest.java

--- src/test/java/ua/en/kosse/oksana/hillel/hw21/HomeWorkClientTest.java
+++ src/test/java/ua/en/kosse/oksana/hillel/hw21/HomeWorkClientTest.java
@@ -1,1 +1,31 @@
+package ua.en.kosse.oksana.hillel.hw21;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ua.en.kosse.oksana.hillel.hw21.client.HomeWorkClient;
+
+
+public class HomeWorkClientTest {
+    @Test
+    public void ClientApp_main_throw_exeption() {
+        Assertions.assertThrows(Exception.class, () -> {
+            HomeWorkClient.main(null);
+        });
+    }
+
+    @Test
+    public void ClientApp_messageReceiver_throw_exeption() {
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            HomeWorkClient.messageReceiver(null);
+        });
+    }
+
+    @Test
+    public void ClientApp_fileCommandProcessor_fileNotFoundMessage() {
+        Assertions.assertEquals(
+                1,
+                HomeWorkClient.fileCommandProcessor(
+                        "file: c:\\path_test", null, null));
+    }
+}
 
